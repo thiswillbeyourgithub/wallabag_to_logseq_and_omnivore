ee06e1b3d8e416fca4d0185af313ba4d371de2c8
thiswillbeyourgithub <26625900+thiswillbeyourgithub@users.noreply.github.com>
2023-08-02 19:58:33 +0200

    minor

--- to_logseq.py
+++ to_logseq.py
@@ -28,43 +28,6 @@ def step1():
 
         os.system(f"wallabag info {entry_id} > exports/{entry_id}_info.md")
 
-def step3():
-    """
-    get the list of id of unread urls
-    """
-    # get all entry id
-    Path("exports/unreads").mkdir(exist_ok=True)
-    os.system("rm exports/unreads/urls.txt")
-    os.system("wallabag list --unread | cut -c-8 > exports/unreads/unread_list.txt")
-    with open("exports/unreads/unread_list.txt", "r") as f:
-        list_content = f.read().split("\n")
-    list_content = [ll for ll in list_content if ll.isdigit()]
-
-    # export the info to get the url
-
-    for entry_id in tqdm(list_content):
-        print(entry_id)
-
-        os.system(f"wallabag info {entry_id} | grep Url | cut -c6- >> exports/unreads/urls.txt")
-
-def step4():
-    """
-    read the unread urls from step3 then create a csv to upload to omnivore via its API
-    """
-    urls = Path("exports/unreads/urls.txt").read_text().split("\n")
-    with open("exports/unreads/urls.csv", "w") as f:
-        f.write("URL,status,labels\n")
-        for url in tqdm(urls):
-            url = url.strip()
-            if not url:
-                continue
-            f.write(f"{url},SUCCEDED,[wallabag_import]\n")
-
-    # upload to omnivore
-    api_key = "redacted"
-    os.system('curl -X POST -H "Authorization: ' + api_key + ' -H "Content-Type: application/json" -d \'{"query":"mutation UploadImportFile($type: UploadImportFileType!, $contentType: String!) { uploadImportFile(type: $type, contentType: $contentType) { ... on UploadImportFileError { errorCodes } ... on UploadImportFileSuccess { uploadSignedUrl } } }","variables":{"type":"URL_LIST","contentType":"text/csv"}}\' "https://api-prod.omnivore.app/api/graphql/api/graphql" | jq -r \'.data.uploadImportFile.uploadSignedUrl\' | \xargs curl -X PUT -H "Content-Type: text/csv" --data-binary "@exports/unreads/url.csv')
-
-
 def step2():
     """
     convert the output from step1 into a markdown logseq file
@@ -165,6 +128,41 @@ def step2():
         with open(sys.argv[1:][0], "w") as f:
             f.write(output)
 
+def step3():
+    """
+    get the list of id of unread urls
+    """
+    # get all entry id
+    Path("exports/unreads").mkdir(exist_ok=True)
+    os.system("rm exports/unreads/urls.txt")
+    os.system("wallabag list --unread | cut -c-8 > exports/unreads/unread_list.txt")
+    with open("exports/unreads/unread_list.txt", "r") as f:
+        list_content = f.read().split("\n")
+    list_content = [ll for ll in list_content if ll.isdigit()]
+
+    # export the info to get the url
+
+    for entry_id in tqdm(list_content):
+        print(entry_id)
+
+        os.system(f"wallabag info {entry_id} | grep Url | cut -c6- >> exports/unreads/urls.txt")
+
+def step4():
+    """
+    read the unread urls from step3 then create a csv to upload to omnivore via its API
+    """
+    urls = Path("exports/unreads/urls.txt").read_text().split("\n")
+    with open("exports/unreads/urls.csv", "w") as f:
+        f.write("URL,status,labels\n")
+        for url in tqdm(urls):
+            url = url.strip()
+            if not url:
+                continue
+            f.write(f"{url},SUCCEDED,[wallabag_import]\n")
+
+    # upload to omnivore
+    api_key = "redacted"
+    os.system('curl -X POST -H "Authorization: ' + api_key + ' -H "Content-Type: application/json" -d \'{"query":"mutation UploadImportFile($type: UploadImportFileType!, $contentType: String!) { uploadImportFile(type: $type, contentType: $contentType) { ... on UploadImportFileError { errorCodes } ... on UploadImportFileSuccess { uploadSignedUrl } } }","variables":{"type":"URL_LIST","contentType":"text/csv"}}\' "https://api-prod.omnivore.app/api/graphql/api/graphql" | jq -r \'.data.uploadImportFile.uploadSignedUrl\' | \xargs curl -X PUT -H "Content-Type: text/csv" --data-binary "@exports/unreads/url.csv')
 
 if __name__ == "__main__":
     #step1()
